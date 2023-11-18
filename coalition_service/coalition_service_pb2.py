a157e276895efd35c2d7a34984dbb530104b4057
Alexsei Popov <popov2895@gmail.com>
2023-11-18 20:35:16 +0300

    change version -> 0.0.3. Change requested field (description->school_user_id)

--- coalition_service/coalition_service_pb2.py
+++ coalition_service/coalition_service_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x17\x63oalition_service.proto\x12\x11\x63oalition_service\"?\n\x19SetCoalitionMemberRequest\x12\r\n\x05login\x18\x01 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x02 \x01(\t\"X\n\x1aSetCoalitionMemberResponse\x12\x0e\n\x06status\x18\x01 \x01(\x05\x12\x13\n\x0b\x64\x65scription\x18\x02 \x01(\t\x12\x15\n\ris_new_member\x18\x03 \x01(\x08\x32\x89\x01\n\x10\x43oalitionService\x12u\n\x14set_coalition_member\x12,.coalition_service.SetCoalitionMemberRequest\x1a-.coalition_service.SetCoalitionMemberResponse\"\x00\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x17\x63oalition_service.proto\x12\x11\x63oalition_service\"B\n\x19SetCoalitionMemberRequest\x12\r\n\x05login\x18\x01 \x01(\t\x12\x16\n\x0eschool_user_id\x18\x02 \x01(\t\"X\n\x1aSetCoalitionMemberResponse\x12\x0e\n\x06status\x18\x01 \x01(\x05\x12\x13\n\x0b\x64\x65scription\x18\x02 \x01(\t\x12\x15\n\ris_new_member\x18\x03 \x01(\x08\x32\x89\x01\n\x10\x43oalitionService\x12u\n\x14set_coalition_member\x12,.coalition_service.SetCoalitionMemberRequest\x1a-.coalition_service.SetCoalitionMemberResponse\"\x00\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -21,9 +21,9 @@ _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'coalition_service_pb2', _gl
 if _descriptor._USE_C_DESCRIPTORS == False:
   DESCRIPTOR._options = None
   _globals['_SETCOALITIONMEMBERREQUEST']._serialized_start=46
-  _globals['_SETCOALITIONMEMBERREQUEST']._serialized_end=109
-  _globals['_SETCOALITIONMEMBERRESPONSE']._serialized_start=111
-  _globals['_SETCOALITIONMEMBERRESPONSE']._serialized_end=199
-  _globals['_COALITIONSERVICE']._serialized_start=202
-  _globals['_COALITIONSERVICE']._serialized_end=339
+  _globals['_SETCOALITIONMEMBERREQUEST']._serialized_end=112
+  _globals['_SETCOALITIONMEMBERRESPONSE']._serialized_start=114
+  _globals['_SETCOALITIONMEMBERRESPONSE']._serialized_end=202
+  _globals['_COALITIONSERVICE']._serialized_start=205
+  _globals['_COALITIONSERVICE']._serialized_end=342
 # @@protoc_insertion_point(module_scope)
